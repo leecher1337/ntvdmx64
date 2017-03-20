fe0f91aadc90b4319f6b382fb5d2af9581cd53d6
leecher1337 <leecher@dose.0wnz.at>
2017-03-20 02:35:34 +0100

    Improved installer, now uses setupapi and therefore offers uninstallation. Additionally, files in system32 directory now get hardlinked so that changes of autoexec.nt and config.nt take effect.

--- ntvdmpatch/release/install.bat
+++ ntvdmpatch/release/install.bat
@@ -1,4 +1,8 @@
 @echo off
+Setlocal EnableDelayedExpansion
+
+if "%1"=="install" goto addappinit
+if "%1"=="uninstall" goto delappinit
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
@@ -37,69 +41,29 @@ for /F "skip=2 tokens=3" %%r in ('reg query HKLM\SYSTEM\CurrentControlSet\Contro
 )
 
 cls
+echo ---------------------------------------------
+echo Installing, please wait...
+echo ---------------------------------------------
+echo Please check for completion-message from installer in taskbar.
+rundll32.exe advpack.dll,LaunchINFSection %CD%\ntvdmx64.inf
+goto fini
 
-echo ---------------------------------------------
-echo Installing symbol server
-echo ---------------------------------------------
-xcopy /s /y /g /k /d /i symbols %SystemRoot%\symbols
-if errorlevel 1 goto errcpy
-util\symfetch %systemRoot%\system32\kernel32.dll %systemRoot%\syswow64\kernel32.dll 
-if errorlevel 1 (
-  echo Downloading required Symbols from Microsoft Symbol server failed.
-  echo It may be that you are not connected to the Internet or there is
-  echo a problem with the symbol server DLLs.
-  echo Please note that you HAVE to fetch the symbols for the loader code to
-  echo work properly. This is not a severe installation error, as the loader
-  echo tries to download the Symbols too, if not available, so ensure that
-  echo you are conneted to the Internet when launching your first DOS 
-  echo Application. You may continue now, or abort and try to install again
-  echo to ensure proper symbol download.
-  pause
-)
-
-echo ---------------------------------------------
-echo Installing ldntvdm.dll loader code
-echo ---------------------------------------------
-move /Y %SystemRoot%\System32\ldntvdm.dll %SystemRoot%\System32\ldntvdm.dll.bak >nul
-move /Y %SystemRoot%\System32\ldntvdm.dll %SystemRoot%\SysWow64\ldntvdm.dll.bak >nul
-xcopy /s /y /g /k /d ldntvdm %SystemRoot%
-if errorlevel 1 goto errcpy
-reg import reg\appinit.reg
+:addappinit
 set AppInit=
 for /F "skip=2 tokens=2*" %%r in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs') do set AppInit=%AppInit%%%s
 echo %AppInit% | findstr /I /C:ldntvdm.dll >nul
 if not "%AppInit%"=="" set AppInit=%AppInit% 
-if errorlevel 1 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /f /d "%AppInit%ldntvdm.dll"
+if errorlevel 1 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /f /d "%AppInit% ldntvdm.dll"
 set AppInit=
-
-echo ---------------------------------------------
-echo Installing registry patches
-echo ---------------------------------------------
-reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\VirtualDeviceDrivers /v VDD >nul
-if errorlevel 1 reg import reg\vdd.reg
-reg import reg\conhost.reg
-reg import reg\wow.reg
-
-echo ---------------------------------------------
-echo Installing DOS and NTVDM
-echo ---------------------------------------------
-xcopy /s /y /g /k DOS %SystemRoot%\system32
-if errorlevel 1 goto errcpy
-xcopy /s /y /g /k DOS %SystemRoot%\SysWow64
-if errorlevel 1 goto errcpy
-
-echo.
-echo =============================================
-echo.
-echo Installation successful.
-echo You now may want to reboot now to get loader code working.
-echo.
 goto fini
 
-:errcpy
-echo Copying to Windows directory failed. Please ensure that you are
-echo running installation procedure as Administrator.
-echo INSTALLATION FAILED
+:delappinit
+set AppInit=
+for /F "skip=2 tokens=2*" %%r in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs') do (
+  for %%t in (%%s) do if not "%%t"=="ldntvdm.dll" set AppInit=!AppInit!%%t 
+)
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /f /d "%AppInit%"
+set AppInit=
+goto fini
 
 :fini
-pause
