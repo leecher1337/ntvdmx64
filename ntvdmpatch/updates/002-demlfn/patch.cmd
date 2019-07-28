be38538ce3b6e4067cf82fbbec99c11dc1d51d20
leecher1337 <leecher@dose.0wnz.at>
2019-07-28 20:05:29 +0200

    Fixes for #53,  forgot that doswow.h should be from Win2k

--- ntvdmpatch/updates/002-demlfn/patch.cmd
+++ ntvdmpatch/updates/002-demlfn/patch.cmd
@@ -2,4 +2,6 @@
 xcopy /S /Y dos W:\nt\private\mvdm\dos
 pushd w:\nt
 ..\ntvdmpatch\util\patch.exe -N -p1 -i ..\ntvdmpatch\updates\002-demlfn\demlfn.patch
+rem In case you don't have Win2k DEMLFN already...
+..\ntvdmpatch\util\patch.exe -N -p1 -i ..\ntvdmpatch\updates\002-demlfn\doswow.patch
 popd
