c3114b98eb1ac9f962ac8e648552a22d4b333e0c
leecher1337 <leecher@dose.0wnz.at>
2019-08-10 02:22:46 +0200

    Adding patchset so that you can finally test new CVIDC implementation

---
+++ ntvdmpatch/experimental/cvidc-new/patch.cmd
@@ -0,0 +1,7 @@
+@echo off
+mkdir W:\nt\private\mvdm\softpc.new\base\cvidc-new
+copy /Y src W:\nt\private\mvdm\softpc.new\base\cvidc-new
+for %%I in (access_c.h ccpudefs.inc cpuint_c.h ev_glue.c evid_c.h evidfunc.c evidfunc.h evidgen.h fm_c.h fpu_c.h gdebug.h gdpvar.h gldc_c.h page_c.h prot_c.h segreg_c.h trace_c.h vglfunc.c) do copy /Y W:\nt\private\mvdm\softpc.new\base\cvidc\%%I W:\nt\private\mvdm\softpc.new\base\cvidc-new
+pushd w:\nt
+..\ntvdmpatch\util\patch.exe -N -p1 -i ..\ntvdmpatch\experimental\cvidc-new\cvidc-new.patch
+popd
