leecher1337/ntvdmx64

commit 929c8533231ce968732a14423c2d48e1bdb4bfe3

Updated Windows 11 loader to fix a severe Bug introduced by Microsoft in Windows 11 affecting conhost(crashes), details in oemcp.c - Is there any way to report bugs to M$?

ntvdmpatch/src/ldntvdm/ldntvdm/oemcp.c
@@ -1,0 +1,171 @@
+/*Project: ldntvdm
+* Module : oemcp
+* Author : leecher@dose.0wnz.at
+* Descr. : The retards at Micro$haft recently removed OemCodePageData pointer 
+*          from PEB in Windows 11. This leads to a crash in ConHost when using
+*          OEM codepage there, because GlyphCP doesn't get initialized due to
+*          the lack of PEB->OemCodePageData.
+*          This in turn leads to crashes in conhost functions: 
+*
+*             ConvertOutputToUnicode
+*             FalseUnicodeToRealUnicode
+*             SB_CharToWcharGlyph
+*
+*          The practical result is that conhost crashes when i.e. not using raster 
+*          font in console, but TTF like "Lucidia Console" and NTVDM application 
+*          runs with OEM codepage.
+*          Thanks for shipping another buggy Windows version, do you guys even do
+*          some basic testing before rolling out the releases?
+* Changes: 01.11.2021  - Created
+*/
+#define WIN32_NO_STATUS
+#include <ntstatus.h>
+#include "ldntvdm.h"
+#include "ntrtl.h"
+#include "ntregapi.h"
+#include "Winternl.h"
+#include "ntpeb.h"
+#include "symcache.h"
+
+#define NLS_TABLE_KEY \
+        L"\\Registry\\Machine\\System\\CurrentControlSet\\Control\\Nls\\CodePage"
+
+typedef BOOL(*pInitializeCustomCP)();
+
+BOOL GetNlsTablePath(
+	UINT CodePage,
+	PWCHAR PathBuffer
+	)
+{
+	NTSTATUS NtStatus;
+	BOOL Result = FALSE;
+	HANDLE RegistryKeyHandle;
+	OBJECT_ATTRIBUTES ObjectAttributes;
+	UNICODE_STRING UnicodeString;
+	PKEY_VALUE_PARTIAL_INFORMATION KeyValueInformation;
+
+	RtlInitUnicodeString(&UnicodeString, NLS_TABLE_KEY);
+
+	InitializeObjectAttributes(&ObjectAttributes,
+		&UnicodeString,
+		OBJ_CASE_INSENSITIVE,
+		NULL,
+		NULL);
+
+	NtStatus = NtOpenKey(&RegistryKeyHandle, GENERIC_READ, &ObjectAttributes);
+
+	if (NT_SUCCESS(NtStatus))
+	{
+		WCHAR ResultBuffer[MAX_PATH + (sizeof(KEY_VALUE_FULL_INFORMATION) / sizeof(WCHAR) + 1)];
+		ULONG ValueReturnedLength, BufferSize = sizeof(ResultBuffer);
+		WCHAR CodePageStringBuffer[20];
+		__swprintf(CodePageStringBuffer, L"%d", CodePage);
+
+		RtlInitUnicodeString(&UnicodeString, CodePageStringBuffer);
+
+		KeyValueInformation = (PKEY_VALUE_PARTIAL_INFORMATION)ResultBuffer;
+
+		NtStatus = NtQueryValueKey(RegistryKeyHandle,
+			&UnicodeString,
+			KeyValuePartialInformation,
+			KeyValueInformation,
+			BufferSize,
+			&BufferSize);
+
+		if (NT_SUCCESS(NtStatus))
+		{
+			__swprintf(PathBuffer + GetSystemDirectoryW(PathBuffer, MAX_PATH), 
+				L"\\%ws",
+				(WCHAR *)&(KeyValueInformation->Data[0]));
+			Result = TRUE;
+		}
+		else
+		{
+			TRACE("GetNlsTablePath failed to get NLS table\n");
+		}
+
+		NtClose(RegistryKeyHandle);
+	}
+	else
+	{
+		TRACE("GetNlsTablePath failed to open NLS key\n");
+	}
+
+	return(Result);
+}
+
+
+BOOL OEMCP_FixNLSTable(void)
+{
+	WCHAR wszPath[MAX_PATH];
+	PPEB_ARCH Peb = NtCurrentPeb();
+
+	if (!Peb->OemCodePageData && GetNlsTablePath(GetOEMCP(), wszPath))
+	{
+		HANDLE hFile;
+		BOOL bRet = FALSE;
+
+		hFile = CreateFileW(wszPath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+		if (hFile != INVALID_HANDLE_VALUE)
+		{
+			HANDLE hMap;
+
+			if (hMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL))
+			{
+				if (Peb->OemCodePageData = (PBYTE)MapViewOfFile(hMap, FILE_MAP_READ, 0, 0, 0))
+				{
+					return TRUE;
+				}
+				CloseHandle(hMap);
+				TRACE("Codepage MapViewOfFile failed: %d\n", GetLastError());
+			}
+			CloseHandle(hFile);
+			TRACE("Codepage CreateFileW failed: %d\n", GetLastError());
+		}
+	}
+	return FALSE;
+}
+
+BOOL OEMCP_CallInitializeCustomCP()
+{
+	DWORD64 dwAddress;
+	HMODULE nt = GetModuleHandleW(L"conhostV1.dll");
+	pInitializeCustomCP fnInitializeCustomCP = NULL;
+#ifdef USE_SYMCACHE
+	NTSTATUS Status;
+	HKEY hKey;
+
+	if (NT_SUCCESS(Status = REG_OpenLDNTVDM(KEY_READ | KEY_WRITE, &hKey)))
+	{
+		if (SymCache_GetDLLKey(hKey, L"conhostV1.dll", FALSE) &&
+			(dwAddress = (DWORD64)SymCache_GetProcAddress(hKey, L"InitializeCustomCP")))
+		{
+			dwAddress += (DWORD64)nt;
+			fnInitializeCustomCP = (pInitializeCustomCP)dwAddress;
+		}
+		REG_CloseKey(hKey);
+	}
+#else
+	DWORD64 dwBase;
+	char szNTDLL[MAX_PATH];
+
+	GetSystemDirectoryA(szNTDLL, sizeof(szNTDLL) / sizeof(szNTDLL[0]));
+	strcat(szNTDLL, "\\conhostV1.dll");
+	if (SymEng_LoadModule(szNTDLL, &dwBase) == 0)
+	{
+		if ((dwAddress = SymEng_GetAddr(dwBase, "InitializeCustomCP")))
+		{
+			dwAddress += (DWORD64)nt;
+			fnInitializeCustomCP = (pInitializeCustomCP)dwAddress;
+		}
+		else
+		{
+			OutputDebugStringA("NTVDM: Resolving symbols failed.");
+		}
+		SymEng_UnloadModule(dwBase);
+	}
+#endif
+	if (!nt || !fnInitializeCustomCP) return FALSE;
+	return fnInitializeCustomCP();
+}

ntvdmpatch/src/ldntvdm/ldntvdm/symcache.c
@@ -74,6 +74,13 @@ static REGKEY_PAIR m_aSymsCONHOST[] =
 	{ NULL, NULL }
 };
 #endif
+#ifdef TARGET_WIN11
+static REGKEY_PAIR m_aSymsCONHOSTV1[] =
+{
+	_E("InitializeCustomCP"),
+	{ NULL, NULL }
+};
+#endif
 #ifdef NEED_APPINFO
 static REGKEY_PAIR m_aSymsAPPINFO[] =
 {
@@ -95,11 +102,14 @@ static REGKEY_SYMS m_aSyms[] = {
 	#if defined(_WIN64) && !defined(TARGET_WINXP) && (defined(TARGET_WIN7) || defined(HOOK_CONHOSTV2)) // consbmp.c fix
 	{"conhost.exe", L"conhost.exe", m_aSymsCONHOST},
 	#endif
+	#ifdef TARGET_WIN11
+	{ "conhostV1.dll", L"conhostV1.dll", m_aSymsCONHOSTV1 },
+	#endif
 	#ifdef NEED_APPINFO
 	{"appinfo.dll", L"appinfo.dll", m_aSymsAPPINFO},
 	#endif
 	#ifdef METHOD_HOOKLDR
-	{"ntdll.dll", L"ntdll.dll", m_aSymsNTDLL}
+	{"ntdll.dll", L"ntdll.dll", m_aSymsNTDLL},
 	#endif
 };
 

ntvdmpatch/src/ldntvdm/ldntvdm/injector64.c
@@ -4,6 +4,7 @@
 #include "injector32.h"
 #include "ntmmapi.h"
 #include "detour.h"
+#include "ntpeb.h"
 #include <stddef.h>
 #include <Psapi.h>
 
@@ -25,91 +26,6 @@ extern BYTE LdrpInitializeProcessProcx86[];
 extern unsigned int LdrpInitializeProcessProcx86Size;
 
 #pragma pack(1)
-typedef struct _PEB32
-{
-	UCHAR				InheritedAddressSpace;				// 0
-	UCHAR				ReadImageFileExecOptions;			// 1
-	UCHAR				BeingDebugged;					// 2
-	union
-	{
-		UCHAR BitField;                                                     //0x3
-		struct
-		{
-			UCHAR ImageUsesLargePages : 1;                                    //0x3
-			UCHAR IsProtectedProcess : 1;                                     //0x3
-			UCHAR IsImageDynamicallyRelocated : 1;                            //0x3
-			UCHAR SkipPatchingUser32Forwarders : 1;                           //0x3
-			UCHAR IsPackagedProcess : 1;                                      //0x3
-			UCHAR IsAppContainer : 1;                                         //0x3
-			UCHAR IsProtectedProcessLight : 1;                                //0x3
-			UCHAR IsLongPathAwareProcess : 1;                                 //0x3
-		};
-	};
-	ULONG				Mutant;						// 4
-	ULONG				ImageBaseAddress;				// 8
-	ULONG				Ldr;							// C
-	ULONG				ProcessParameters;				// 10
-	ULONG				SubSystemData;
-	ULONG				ProcessHeap;
-	ULONG				FastPebLock;
-	ULONG				AtlThunkSListPtr;
-	ULONG				IFEOKey;
-	union
-	{
-		ULONG CrossProcessFlags;
-		struct
-		{
-			ULONG ProcessInJob : 1;
-			ULONG ProcessInitializing : 1;
-			ULONG ProcessUsingVEH : 1;
-			ULONG ProcessUsingVCH : 1;
-			ULONG ReservedBits0 : 28;
-		};
-	};
-	union
-	{
-		ULONG KernelCallbackTable;                                          //0x2c
-		ULONG UserSharedInfoPtr;                                            //0x2c
-	};
-	ULONG SystemReserved;                                                   //0x30
-	ULONG AtlThunkSListPtr32;                                               //0x34
-	ULONG ApiSetMap;                                                        //0x38
-	ULONG TlsExpansionCounter;                                              //0x3c
-	ULONG TlsBitmap;                                                        //0x40
-	ULONG TlsBitmapBits[2];                                                 //0x44
-	ULONG ReadOnlySharedMemoryBase;                                         //0x4c
-	ULONG SharedData;                                                       //0x50
-	ULONG ReadOnlyStaticServerData;                                         //0x54
-	ULONG AnsiCodePageData;                                                 //0x58
-	ULONG OemCodePageData;                                                  //0x5c
-	ULONG UnicodeCaseTableData;                                             //0x60
-	ULONG NumberOfProcessors;                                               //0x64
-	ULONG NtGlobalFlag;                                                     //0x68
-	union _LARGE_INTEGER CriticalSectionTimeout;                            //0x70
-	ULONG HeapSegmentReserve;                                               //0x78
-	ULONG HeapSegmentCommit;                                                //0x7c
-	ULONG HeapDeCommitTotalFreeThreshold;                                   //0x80
-	ULONG HeapDeCommitFreeBlockThreshold;                                   //0x84
-	ULONG NumberOfHeaps;                                                    //0x88
-	ULONG MaximumNumberOfHeaps;                                             //0x8c
-	ULONG ProcessHeaps;                                                     //0x90
-	ULONG GdiSharedHandleTable;                                             //0x94
-	ULONG ProcessStarterHelper;                                             //0x98
-	ULONG GdiDCAttributeList;                                               //0x9c
-	ULONG LoaderLock;                                                       //0xa0
-	ULONG OSMajorVersion;                                                   //0xa4
-	ULONG OSMinorVersion;                                                   //0xa8
-	USHORT OSBuildNumber;                                                   //0xac
-	USHORT OSCSDVersion;                                                    //0xae
-	ULONG OSPlatformId;                                                     //0xb0
-	ULONG ImageSubsystem;                                                   //0xb4
-	ULONG ImageSubsystemMajorVersion;                                       //0xb8
-	ULONG ImageSubsystemMinorVersion;                                       //0xbc
-	ULONG ActiveProcessAffinityMask;                                        //0xc0
-	ULONG GdiHandleBuffer[34];                                              //0xc4
-	ULONG PostProcessInitRoutine;                                           //0x14c
-} PEB32, *PPEB32;
-
 typedef struct _LDR_DATA_TABLE_ENTRY32
 {
 	LIST_ENTRY32 InLoadOrderModuleList;

ntvdmpatch/src/ldntvdm/ldntvdm/ldntvdm.c
@@ -40,6 +40,7 @@
 #include "apppatch.h"
 #include "appinfo.h"
 #include "injector32.h"
+#include "oemcp.h"
 
 #pragma comment(lib, "ntdll.lib")
 
@@ -192,11 +193,11 @@ fpsprintf sprintf;
 fp_stricmp __stricmp;
 fp_wcsicmp __wcsicmp;
 fpstrcmp _strcmp;
+fpswprintf __swprintf;
 #ifdef NEED_BASEVDM
 fpwcsncpy _wcsncpy;
 fp_wcsnicmp __wcsnicmp;
 fpwcsrchr _wcsrchr;
-fpswprintf __swprintf;
 fpstrstr _strstr;
 #endif
 fpBaseIsDosApplication BaseIsDosApplication = NULL;
@@ -637,6 +638,7 @@ BOOL WINAPI _DllMainCRTStartup(
 		__stricmp = (fp_stricmp)GetProcAddress(hNTDLL, "_stricmp");
 		__wcsicmp = (fp_wcsicmp)GetProcAddress(hNTDLL, "_wcsicmp");
 		_strcmp = (fpstrcmp)GetProcAddress(hNTDLL, "strcmp");
+		__swprintf = (fpswprintf)GetProcAddress(hNTDLL, "swprintf");
 #ifdef TRACING
 		sprintf = (fpsprintf)GetProcAddress(hNTDLL, "sprintf");
 #ifdef TRACE_FILE
@@ -652,7 +654,6 @@ BOOL WINAPI _DllMainCRTStartup(
 		_wcsncpy = (fpwcsncpy)GetProcAddress(hNTDLL, "wcsncpy");
 		__wcsnicmp = (fp_wcsnicmp)GetProcAddress(hNTDLL, "_wcsnicmp");
 		_wcsrchr = (fpwcsrchr)GetProcAddress(hNTDLL, "wcsrchr");
-		__swprintf = (fpswprintf)GetProcAddress(hNTDLL, "swprintf");
 		 _strstr = (fpstrstr)GetProcAddress(hNTDLL, "strstr");
 #endif
 
@@ -766,6 +767,16 @@ BOOL WINAPI _DllMainCRTStartup(
 			FixNTDLL();
 			// Fix ConhostV1.dll bug where memory isn't initialized properly
 			fNoConhostDll = ConsBmpBug_Install(&hModConhost);
+
+#ifdef TARGET_WIN11
+			// Windows 11 bug: OEM NLS Table not mapped
+			if (OEMCP_FixNLSTable())
+			{
+				//SuspendThread(GetCurrentThread());
+				OEMCP_CallInitializeCustomCP();
+			}
+#endif
+
 #ifndef CREATEPROCESS_HOOK
 			// We want notification when new console process gets started so that we can inject
 			WinEventHook_Install(fNoConhostDll ? GetModuleHandle(NULL) : hModConhost);

ntvdmpatch/src/ldntvdm/ldntvdm/ldntvdm.h
@@ -136,19 +136,19 @@ void Trace(char *pszLine);
 typedef int (__cdecl *fpstrcmp)(char * str, char *str2);
 typedef int (__cdecl *fp_stricmp)(const char * str, const char *str2);
 typedef int (__cdecl *fp_wcsicmp)(PWCHAR str, PWCHAR str2);
+typedef int(__cdecl *fpswprintf)(wchar_t *buffer, const wchar_t *format, ...);
 extern fp_stricmp __stricmp;
 extern fp_wcsicmp __wcsicmp;
 extern fpstrcmp _strcmp;
+extern fpswprintf __swprintf;
 #ifdef NEED_BASEVDM
 typedef wchar_t * (__cdecl *fpwcsncpy)(wchar_t *strDest, const wchar_t *strSource, size_t count);
 typedef int(__cdecl *fp_wcsnicmp)(const wchar_t *string1, const wchar_t *string2, size_t count);
 typedef wchar_t * (__cdecl *fpwcsrchr)(const wchar_t *str, wchar_t c);
-typedef int (__cdecl *fpswprintf)(wchar_t *buffer, const wchar_t *format, ...);
 typedef char *(__cdecl *fpstrstr)(const char *str, const char *strSearch);
 extern fpwcsncpy _wcsncpy;
 extern fp_wcsnicmp __wcsnicmp;
 extern fpwcsrchr _wcsrchr;
-extern fpswprintf __swprintf;
 extern fpstrstr _strstr;
 #endif
 

ntvdmpatch/src/ldntvdm/ldntvdm/ldntvdm.vcxproj
@@ -545,6 +545,7 @@
     <ClCompile Include="injector32.c" />
     <ClCompile Include="injector64.c" />
     <ClCompile Include="ldntvdm.c" />
+    <ClCompile Include="oemcp.c" />
     <ClCompile Include="reg.c" />
     <ClCompile Include="symcache.c" />
     <ClCompile Include="symeng.c" />
@@ -577,9 +578,11 @@
     <ClInclude Include="newexe.h" />
     <ClInclude Include="newres.h" />
     <ClInclude Include="ntmmapi.h" />
+    <ClInclude Include="ntpeb.h" />
     <ClInclude Include="ntpsapi.h" />
     <ClInclude Include="ntregapi.h" />
     <ClInclude Include="ntrtl.h" />
+    <ClInclude Include="oemcp.h" />
     <ClInclude Include="reg.h" />
     <ClInclude Include="symcache.h" />
     <ClInclude Include="symeng.h" />

ntvdmpatch/src/ldntvdm/ldntvdm/ldntvdm.vcxproj.filters
@@ -93,6 +93,9 @@
     <ClCompile Include="appinfo.c">
       <Filter>Quelldateien</Filter>
     </ClCompile>
+    <ClCompile Include="oemcp.c">
+      <Filter>Quelldateien</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="basemsg64.h">
@@ -201,6 +204,12 @@
       <Filter>Headerdateien</Filter>
     </ClInclude>
     <ClInclude Include="injector.h">
+      <Filter>Headerdateien</Filter>
+    </ClInclude>
+    <ClInclude Include="ntpeb.h">
+      <Filter>Headerdateien</Filter>
+    </ClInclude>
+    <ClInclude Include="oemcp.h">
       <Filter>Headerdateien</Filter>
     </ClInclude>
   </ItemGroup>

commit 62b5db79bd8334f25fcd7c466b1e3441ba84a6b5

Fixed a bug introduced with 929c8533231ce968732a14423c2d48e1bdb4bfe3 in Windows 11 loader that accidentally tried to lookup conhostv1.dll symbols also in 32bit loader which caused hangs and various crashes #158

ntvdmpatch/src/ldntvdm/ldntvdm/oemcp.c
@@ -127,6 +127,7 @@ BOOL OEMCP_FixNLSTable(void)
 	return FALSE;
 }
 
+#ifdef _WIN64
 BOOL OEMCP_CallInitializeCustomCP()
 {
 	DWORD64 dwAddress;
@@ -169,3 +170,4 @@ BOOL OEMCP_CallInitializeCustomCP()
 	if (!nt || !fnInitializeCustomCP) return FALSE;
 	return fnInitializeCustomCP();
 }
+#endif

ntvdmpatch/src/ldntvdm/ldntvdm/symcache.c
@@ -102,7 +102,7 @@ static REGKEY_SYMS m_aSyms[] = {
 	#if defined(_WIN64) && !defined(TARGET_WINXP) && (defined(TARGET_WIN7) || defined(HOOK_CONHOSTV2)) // consbmp.c fix
 	{"conhost.exe", L"conhost.exe", m_aSymsCONHOST},
 	#endif
-	#ifdef TARGET_WIN11
+	#if defined(TARGET_WIN11) && defined(_WIN64)
 	{ "conhostV1.dll", L"conhostV1.dll", m_aSymsCONHOSTV1 },
 	#endif
 	#ifdef NEED_APPINFO