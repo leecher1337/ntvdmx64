ee692f1a267a30b255b17b05bf0a704931b96da3
leecher1337 <leecher@dose.0wnz.at>
2024-11-02 23:14:49 +0100

    Make symbol installer more convenient (so that it runs as admin and is able to copy symbol files)

--- ntvdmpatch/minnt/release/inst-sym.cmd
+++ ntvdmpatch/minnt/release/inst-sym.cmd
@@ -1,1 +1,19 @@
-@rundll32.exe advpack.dll,LaunchINFSection %CD%\ntvdmdbg.inf
+@echo off
+
+:StartExec
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+  echo Requesting administrative privileges...
+  goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+rundll32.exe advpack.dll,LaunchINFSection %CD%\ntvdmdbg.inf
